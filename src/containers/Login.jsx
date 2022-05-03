33325aa2b228541f1ae6dc42ccf8d58605a8e225
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-03 14:57:43 -0300

    feat: containers/ folder created.

---
+++ src/containers/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <main className="container">
+      <figure className="logo__container">
+        <img className="logo" src="./logos/logo_yard_sale.svg" alt="Yard Sale logo" />
+      </figure>
+
+      <div className="form__container">
+        <div className="form__header">
+          <h1 className="title">Create a new password</h1>
+          <p className="subtitle">Enter a new password for your account.</p>
+        </div>
+
+        <form className="form" action="/">
+          <label className="label" for="new-password">Password</label>
+          <input className="input input__new-password" type="password" id="new-password" placeholder="********" required />
+
+          <label className="label" for="re-enter-password">Re-enter password</label>
+          <input className="input input__new-password" type="password" id="re-enter-password" placeholder="********" required />
+
+          <input className="primary__btn new-password__btn" type="submit" value="Confirm new password" />
+        </form>
+      </div>
+    </main>
+  );
+}
+
+export default Login;

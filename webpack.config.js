5bdd6f316be5b151a108240d422d569a6b7f4651
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-03 15:37:50 -0300

    feat: new containers alias added.

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
         extensions: ['.js', '.jsx'],
         alias: {
             '@components': path.resolve(__dirname, 'src/components/'),
+            '@containers': path.resolve(__dirname, 'src/containers/'),
             '@styles': path.resolve(__dirname, 'src/styles/'),
         }
     },

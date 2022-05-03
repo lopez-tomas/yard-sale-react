df0740248624b5eecfce10f56a2b98b628aeb88e
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-03 14:42:57 -0300

    feat: styles/ folder added & using global styles.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import '@styles/global.sass';
 
 const App = () => {
     return (
-        <h1>Hello World!</h1>
+        <h1>Hello World! Welcome to Yard Sale</h1>
     );
 }
 

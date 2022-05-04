708a85fa910225114a781d123b2f8a7fd15d5e56
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-04 19:45:54 -0300

    feat: minor change.

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from '@pages/Home.jsx';
-import NotFound from '@pages/NotFound.jsx';
+import Home from '@pages/Home';
+import NotFound from '@pages/NotFound';
 import Layout from '@containers/Layout';
-import Login from '@containers/Login';
+import NewPassword from '@containers/NewPassword';
 import PasswordRecovery from '@containers/PasswordRecovery';
 
 const App = () => {
   return (
     <Router>
+      <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/new-password" element={
-            <Layout>
-              <Login />
-            </Layout>
-          } />
-          <Route path="/password-recovery" element={
-            <Layout>
-              <PasswordRecovery />
-            </Layout>
-          } />
+          <Route path="/new-password" element={<NewPassword />} />
+          <Route path="/password-recovery" element={<PasswordRecovery />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+      </Layout>
     </Router>
   );
 }

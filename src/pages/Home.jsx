b2f907669cfd3e467a4c644c0ac4c41c2d0e7faa
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-05 12:19:39 -0300

    feat: Styles add to Home page and showing Product component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import Header from '@components/Header';
+import Product from '@components/Product';
+import '@styles/Home.sass';
 
 const Home = () => {
   return (
-    <Header />
+    <body>
+      <Header />
+
+      <main class="container">
+        <section className="cards__container">
+          <Product />
+          <Product />
+          <Product />
+        </section>
+      </main>
+    </body>
   )
 }
 

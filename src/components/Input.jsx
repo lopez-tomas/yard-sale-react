3ba2a115666b94067bdf21112fef8f9a3d8b296b
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-06 16:55:27 -0300

    [feat]: getting className & adding it to <button>.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,14 +3,17 @@ import '@styles/modules/_inputs.sass';
 import '@styles/modules/_primary-btn.sass';
 import '@styles/modules/_secondary-btn.sass';
 
-const Input = ({ type, id, placeholder, value, disabled = false, secondary = false }) => {
+const Input = ({ className, type, id, placeholder, value, disabled = false, secondary = false }) => {
   if (type != "submit") {
+    let style = (className) ? `input ${className}` : "input";
+    style = (disabled) ? `${style} disabled` : `${style}`;
     return (
-      <input className={disabled ? "input disabled" : "input"} type={type} id={id} placeholder={placeholder} value={value} disabled={disabled} required />
+      <input className={style} type={type} id={id} placeholder={placeholder} value={value} disabled={disabled} required />
     )
   } else {
+    let style = (secondary) ? `secondary__btn ${className}` : `primary__btn ${className}`;
     return (
-      <input className={secondary ? "secondary__btn" : "primary__btn"} type={type} value={value} />
+      <input className={style} type={type} value={value} />
     )
   }
 }

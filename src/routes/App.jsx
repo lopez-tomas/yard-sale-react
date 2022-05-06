757a0ec6ac503a89fd47278efc5143a76dfea090
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-06 17:20:42 -0300

    [feat]: using HeaderLayout.

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Layout from '@containers/Layout';
+import HeaderLayout from '@containers/HeaderLayout';
 import Home from '@pages/Home';
+import Login from '@pages/Login';
 import MyAccount from '@pages/MyAccount';
 import NewPassword from '@pages/NewPassword';
 import PasswordRecovery from '@pages/PasswordRecovery';
@@ -13,17 +15,40 @@ import NotFound from '@pages/NotFound';
 const App = () => {
   return (
     <Router>
-      {/* <Layout> */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={
+            <HeaderLayout>
+              <Home />
+            </HeaderLayout>
+          } />
+          <Route path="/login" element={
+            <HeaderLayout>
+              <Login />
+            </HeaderLayout>
+          } />
+          <Route path="/my-account" element={
+            <HeaderLayout>
+              <MyAccount />
+            </HeaderLayout>
+          } />
+          <Route path="/new-password" element={
+            <HeaderLayout>
+              <NewPassword />
+            </HeaderLayout>
+          } />
+          <Route path="/password-recovery" element={
+            <HeaderLayout>
+              <PasswordRecovery />
+            </HeaderLayout>
+          } />
+          <Route path="/password-recovery-success" element={
+            <HeaderLayout>
+              <PasswordRecoverySuccess />
+            </HeaderLayout>
+          } />
           <Route path="/product-details" element={<ProductDetails />} />
-          <Route path="/my-account" element={<MyAccount />} />
-          <Route path="/new-password" element={<NewPassword />} />
-          <Route path="/password-recovery" element={<PasswordRecovery />} />
-          <Route path="/password-recovery-success" element={<PasswordRecoverySuccess />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      {/* </Layout> */}
     </Router>
   );
 }

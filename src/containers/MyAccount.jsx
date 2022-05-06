0de648f3a09129cf8d88331afdd2334a5731f4bf
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-06 13:37:19 -0300

    feat: new container <MyAccount> created & added to routes.

---
+++ src/containers/MyAccount.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Label from '@components/Label';
+import Input from '@components/Input';
+
+const MyAccount = () => {
+  return (
+    <main class="container">
+      <div class="form__container">
+        <h1 class="title">My account</h1>
+
+        <form class="form" action="/">
+          <div>
+            <Label to="name">Name</Label><br />
+            <Input type="text" id="name" placeholder="Your name" value="Tomás López" disabled /><br />
+
+            <Label to="email">Email address</Label><br />
+            <Input type="email" id="email" placeholder="Your email" value="tomaslopez@gmail.com" disabled /><br />
+
+            <Label to="password">Password</Label><br />
+            <Input type="password" id="password" placeholder="Your password" value="********" disabled />
+          </div>
+
+          <Input type="submit" value="Edit" secondary />
+          {/* <input class="secondary__btn edit__btn" type="submit" value="Edit" /> */}
+        </form>
+      </div>
+    </main>
+  )
+}
+
+export default MyAccount;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Layout from '@containers/Layout';
 import Home from '@pages/Home';
+import MyAccount from '@containers/MyAccount';
 import NewPassword from '@containers/NewPassword';
 import PasswordRecovery from '@containers/PasswordRecovery';
 import NotFound from '@pages/NotFound';
@@ -13,6 +14,7 @@ const App = () => {
       {/* <Layout> */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/my-account" element={<MyAccount />} />
           <Route path="/new-password" element={<NewPassword />} />
           <Route path="/password-recovery" element={<PasswordRecovery />} />
           <Route path="*" element={<NotFound />} />

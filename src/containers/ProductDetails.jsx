f465be0ed4686dfe2bf3d31f527c2d2f201a4632
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-06 15:58:08 -0300

    feat: using Figure & Button components.

--- src/containers/ProductDetails.jsx
+++ src/containers/ProductDetails.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Figure from '@components/Figure';
+import Button from '@components/Button';
 import '@styles/ProductDetails.sass';
 
 const ProductDetails = () => {
   return (
     <aside class="product-detail">
-      <figure class="product-image__container">
-        <img class="product-image" src="./images/bike.jpeg" alt="Product" />
-      </figure>
+      <Figure figureClass="product-image__container" imageClass="product-image" src="./images/bike.jpeg" alt="Product" />
 
       <div class="product-info">
         <div>
@@ -22,10 +22,10 @@ const ProductDetails = () => {
           </p>
         </div>
 
-        <button class="primary__btn add__button" type="submit">
+        <Button>
           <img src="./icons/bt_add_to_cart.svg" alt="Add to cart" />
           Add to cart
-        </button>
+        </Button>
       </div>
 	</aside>
   )

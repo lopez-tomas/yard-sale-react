6fd79a046bb87039f49ee6419c24ab3286f6d3b1
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-09 19:01:10 -0300

    [feat]: getting products from API & showing them. ProductItem component receives title, price & image now.

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ProductItem from '@components/ProductItem';
 import '@styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products';
+
 const ProductList = () => {
+	const [products, setProducts] = useState([]);
+
+	const getProducts = async () => {
+		const response = await axios.get(`${API}?limit=5&offset=1`);
+		setProducts(response.data);
+	}
+
+	useEffect(() => {
+		getProducts();
+	}, [])
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem
+						key={product.id}
+						title={product.title}
+						price={product.price}
+						image={product.images[0]} />
+				))}
 			</div>
 		</section>
 	);

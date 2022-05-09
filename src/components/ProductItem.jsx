537efcf8891a09ac7f5001164cb2cd357c8ca10a
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-09 19:01:37 -0300

    [feat/component]: component receives title, price & image to show.

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 import addToCartIcon from '@icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
+const ProductItem = ({ title, price, image }) => {
 	const [message, setMessage] = useState('');
 	const [cart, setCart] = useState(false);
 
 	const handleMessage = () => {
 		if (!cart) {
-			setMessage('Product added to cart');
+			setMessage('Added to cart');
 		} else {
-			setMessage('Product removed from cart');
+			setMessage('Removed from cart');
 		}
 	}
 
@@ -21,14 +21,14 @@ const ProductItem = () => {
 
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={image} alt={title} />
 			<div>
 				{message}
 			</div>
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${price}</p>
+					<p>{title}</p>
 				</div>
 				<figure onClick={handleCart}>
 					<img src={addToCartIcon} alt="" />

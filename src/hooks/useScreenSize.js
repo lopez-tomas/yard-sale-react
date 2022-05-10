95a55f7d4d653ed9a0a7c07fa30d095cf272d279
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-10 18:27:06 -0300

    [feat/hook]: new hook - useScreenSize - created. It returns window dimension and a function to detect screen height & width.

---
+++ src/hooks/useScreenSize.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+
+const useScreenSize = () => {
+  const [windowDimension, detectHW] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+  })
+
+  const detectSize = () => {
+    detectHW({
+      winWidth: window.innerWidth,
+      winHeight: window.innerHeight,
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', detectSize)
+
+    return () => {
+      window.removeEventListener('resize', detectSize)
+    }
+  }, [windowDimension])
+
+  return {
+    windowDimension,
+    detectHW,
+  }
+};
+
+export default useScreenSize;

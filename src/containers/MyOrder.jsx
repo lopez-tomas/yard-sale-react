65a4047a89656b7e8d4ea2ad4089559cef292d5c
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-10 12:26:51 -0300

    [feat/context]: using AppContext to get state.cart and show each product added to shopping cart.

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '@context/AppContext';
 import OrderItem from '@components/OrderItem';
 import '@styles/MyOrder.scss';
 import arrowIcon from '@icons/flechita.svg';
 
 const MyOrder = () => {
+	const { state } = useContext(AppContext);
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
@@ -11,7 +13,11 @@ const MyOrder = () => {
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				<OrderItem />
+				{state.cart.map(product => (
+					<OrderItem
+						key={`orderItem-${product.id}`}
+						product={product} />
+				))}
 				<div className="order">
 					<p>
 						<span>Total</span>

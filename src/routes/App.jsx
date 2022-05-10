3349135693571e0d2fe10615c9d2341a29c69252
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-10 11:25:05 -0300

    [feat]: using AppContext & useInitialState hook to create a Provider.

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppContext from '@context/AppContext';
+import useInitialState from '@hooks/useInitialState';
 import Layout from '@containers/Layout';
 import Home from '@pages/Home';
 import Login from '@pages/Login';
@@ -14,23 +16,26 @@ import NotFound from '@pages/NotFound';
 import '@styles/global.css';
 
 const App = () => {
+	const initialState = useInitialState();
 	return (
-		<BrowserRouter>
-			<Layout>
-				<Routes>
-					<Route exact path="/" element={<Home />} />
-					<Route exact path="/login" element={<Login />} />
-					<Route exact path="/password-recovery" element={<PasswordRecovery />} />
-					<Route exact path="/send-email" element={<SendEmail />} />
-					<Route exact path="/new-password" element={<NewPassword />} />
-					<Route exact path="/account" element={<MyAccount />} />
-					<Route exact path="/signup" element={<CreateAccount />} />
-					<Route exact path="/checkout" element={<Checkout />} />
-					<Route exact path="/orders" element={<Orders />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</Layout>
-		</BrowserRouter>
+		<AppContext.Provider value={initialState}>
+			<BrowserRouter>
+				<Layout>
+					<Routes>
+						<Route exact path="/" element={<Home />} />
+						<Route exact path="/login" element={<Login />} />
+						<Route exact path="/password-recovery" element={<PasswordRecovery />} />
+						<Route exact path="/send-email" element={<SendEmail />} />
+						<Route exact path="/new-password" element={<NewPassword />} />
+						<Route exact path="/account" element={<MyAccount />} />
+						<Route exact path="/signup" element={<CreateAccount />} />
+						<Route exact path="/checkout" element={<Checkout />} />
+						<Route exact path="/orders" element={<Orders />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Layout>
+			</BrowserRouter>
+		</AppContext.Provider>
 	);
 }
 

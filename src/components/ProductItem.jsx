d6b7fef5afd2bc19ce32dc3e6f88daa17b20fb03
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-12 14:49:19 -0300

    [FEAT]: using default image when product.images[0] is not defined. New function -- isProductAdded() -- added which returns TRUE if current product is in cart if not returns FALSE. Using this function to: 1) add OR remove product in cart - 2) change cart icon between addToCartIcon & addedToCartIcon.

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,25 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import AppContext from '@context/AppContext';
 import '@styles/ProductItem.scss';
 import addToCartIcon from '@icons/bt_add_to_cart.svg';
+import addedToCartIcon from '@icons/bt_added_to_cart.svg';
+import defaultImage from '@images/bike.jpeg';
 
 const ProductItem = ({ product }) => {
-	const { addToCart } = useContext(AppContext);
+	const { state, addToCart, removeFromCart } = useContext(AppContext);
+
+	const isProductAdded = () => state.cart.some(item => item.id == product.id);
 
 	const handleClick = item => {
-		addToCart(item);
+		isProductAdded() ? removeFromCart(item.id) : addToCart(item);
 	}
+
+	const image = product.images[0] ? product.images[0] : defaultImage;
+	const imageAlt = product.images[0] ? product.title : 'Default image';
 
 	return (
 		<div className="ProductItem">
-			<img src={product.images[0]} alt={product.title} />
+			<img src={image} alt={imageAlt} />
 			<div className="product-info">
 				<div>
 					<p>${product.price}</p>
 					<p>{product.title}</p>
 				</div>
 				<figure onClick={() => handleClick(product)}>
-					<img src={addToCartIcon} alt="" />
+					<img
+						src={!isProductAdded() ? addToCartIcon : addedToCartIcon}
+						alt={!isProductAdded() ? 'Add to cart icon' : 'Added to cart icon'}
+					/>
 				</figure>
 			</div>
 		</div>

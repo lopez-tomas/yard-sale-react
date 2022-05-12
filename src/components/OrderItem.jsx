e3b2ae5bf245e8c4655dc20c3244314d184b62f8
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-12 14:44:01 -0300

    [FEAT]: using a default image when product.images[0] is not defined. handleRemove() now receives product.id

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,19 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import AppContext from '@context/AppContext';
 import '@styles/OrderItem.scss';
 import closeIcon from '@icons/icon_close.png';
+import defaultImage from '@images/bike.jpeg';
 
-const OrderItem = ({ indexValue, product }) => {
+const OrderItem = ({ product }) => {
 	const { removeFromCart } = useContext(AppContext);
 
-	const handleRemove = (index) => {
-		removeFromCart(index);
+	const image = product.images[0] ? product.images[0] : defaultImage;
+	const imageAlt = product.images[0] ? product.title : 'Default image';
+
+	const handleRemove = (id) => {
+		removeFromCart(id);
 	};
 
 	return (
 		<div className="OrderItem">
 			<figure>
-				<img src={product.images[0]} alt={product.title} />
+				<img src={image} alt={imageAlt} />
 			</figure>
 			<p>{product.title}</p>
 			<p>${product.price}</p>
@@ -21,7 +25,7 @@ const OrderItem = ({ indexValue, product }) => {
 				className="removeIcon"
 				src={closeIcon}
 				alt="close"
-				onClick={() => handleRemove(indexValue)}
+				onClick={() => handleRemove(product.id)}
 			/>
 		</div>
 	);
